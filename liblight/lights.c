7d78e0d24d4b124577553b55a5a5bb1d31df44aa
Unknown <unterwelt1337@yahoo.de>
2017-12-04 18:04:42 +0100

    refactored adapt_colors_for_blink
    fixes for some values
    
    Change-Id: I77fff126348ffa76bd0f4a36107796fa170125a3

--- liblight/lights.c
+++ liblight/lights.c
@@ -127,6 +127,104 @@ set_light_backlight(struct light_device_t* dev,
 }
 
 static int
+get_fade_index(int base, int num, int shift_fade)
+{
+    if (num == 0) return 0; // we can ignore zeroes
+    if ((base % num) != 0) return -1; // when values is not divisible
+                                      // we cannot synchronize them
+    int division = base / num; 
+
+    int max_fade = 6; // 2^6, can be setted to 7
+    for (int i = 0; i <= max_fade; i++) {
+        if (division == (int)pow(2, i)) // values need to be divided with 2^x
+            return i + shift_fade; // shift_fade increases default fade value 
+    }
+
+    return -1;
+}
+
+static int
+match_color_grid_value(int value)
+{   // to do: revert array and reimplement for-cycle for it
+    const int grid[] = {16, 32, 64, 128, 256}; // we can add 2 more values, but these are enough
+    const float diff_rate_great = 0.5f; // round down (64 + 64*0.5 =round_to=> 64) 
+    const float diff_rate_less = 0.25f; // round up (64 - 64*0.25 + 1 =round_to=> 64)
+    const int   zero_like = 12; // 16 - 16 * 0.25
+
+    if (zero_like >= value) return 0;
+
+    for (int index = 0; index != sizeof(grid)/sizeof(int); index++) { // to do: optimize
+        int diff_great = grid[index] * diff_rate_great;
+        int diff_less  = grid[index] * diff_rate_less;
+
+        if (grid[index] + diff_great >= value)
+            return grid[index];
+    }
+
+    // value is more than maximum
+    return grid[sizeof(grid)/sizeof(int) - 1]; // the last element
+}
+
+static int
+min_not_zero(int* values, int size)
+{
+    int min = 0;
+
+    for (int i = 0; i != size; i++)
+        if (0 < values[i] && values[i] < min) 
+            min = values[i];
+
+    return min;
+}
+
+static void
+adapt_colors_for_blink(int* red, int* green, int* blue)
+{
+    int part[]  = {(*red), (*green), (*blue)}; // color components
+    int round[] = {     0,        0,       0}; // rounded to grid
+    int rate[]  = {     0,        0,       0}; // minimum component * rate = true component value 
+    const int size = sizeof(part)/sizeof(int); // 3
+    int max, min, min_round;
+    float max_rate, revert_rate;
+
+    // find max element
+    max = part[0];
+    for (int i = 0; i != size; i++) 
+        if (max < part[i]) max = part[i];
+
+    if (max == 0) return; // 0 0 0
+
+    max_rate    =       256.f / (float) max; // max * max_rate = 256
+    revert_rate = (float) max / 256.f;       //(max * max_rate) * revert_rate = max
+
+    for (int i = 0; i != size; i++) {
+        part[i] = (int)((float)part[i] * max_rate); // max number will be 256,
+                                                    // increase elements proportionally
+        round[i] = match_color_grid_value(part[i]); // round off values to the grid
+
+        part[i] = round[i] * revert_rate; // return the original scale of values
+    }
+
+    min       = min_not_zero(part,  size);
+    min_round = min_not_zero(round, size);
+
+    if (min_round > 0) {
+
+        for (int i = 0; i != size; i++) {
+            rate[i] = round[i] / min_round; // calculate rates of components
+
+            part[i] = min * rate[i];        // min * rate = true component value 
+        }
+
+    }
+
+    // result output
+    *red   = part[0];
+    *green = part[1];
+    *blue  = part[2];
+}
+
+static int
 set_speaker_light_locked(struct light_device_t* dev,
         struct light_state_t const* state)
 {
@@ -162,34 +260,50 @@ set_speaker_light_locked(struct light_device_t* dev,
     green = (colorRGB >> 8) & 0xFF;
     blue = colorRGB & 0xFF;
 
-    if (onMS > 0 && offMS > 0) {
-        /*
-         * if ON time == OFF time
-         *   use blink mode 2
-         * else
-         *   use blink mode 1
-         */
-        if (onMS == offMS)
-            blink = 2;
-        else
-            blink = 1;
-    } else {
-        blink = 0;
-    }
+    float a_rate = alpha / 256.f;
 
     if (blink) {
-        if (red) {
-            if (write_int(RED_BLINK_FILE, blink))
-                write_int(RED_LED_FILE, 0);
-	}
-        if (green) {
-            if (write_int(GREEN_BLINK_FILE, blink))
-                write_int(GREEN_LED_FILE, 0);
-	}
-        if (blue) {
-            if (write_int(BLUE_BLINK_FILE, blink))
-                write_int(BLUE_LED_FILE, 0);
-	}
+
+        adapt_colors_for_blink(&red, &green, &blue); 
+        
+        // In our case, use the settings in the driver led range values
+        // to do: refactor intervals
+        if (onMS < 1000)
+            onMS = 1000;
+        else if (onMS > 5000)
+            onMS = 5000;
+
+        if (offMS < 1000)
+            offMS = 1000;
+        else if (offMS > 7000)
+            offMS = 7000;
+        
+        // Indexes [0: 0.13, 1:0.26, 2: 0.52, 3:1.04, 4: 2.08, 5: 4.16, 6: 8.32, 7: 16.64]
+        // max fade grid     256     128      64      32       16       8        4   
+        // to do: match max fade element to grid
+        
+        max = (red > green) ? ((red > blue) ? red : blue) : ((green > blue) ? green : blue);
+        red_fade   = get_fade_index(max, red,   1); // rise and fall = 0.26
+        green_fade = get_fade_index(max, green, 1);
+        blue_fade  = get_fade_index(max, blue,  1);
+
+        ALOGD("set_speaker_light_locked r %d, g %d, b %d, rf %d, gf %d, bf %d\n",
+            red, green, blue, red_fade, green_fade, blue_fade);
+
+        if (red_fade == -1 || green_fade == -1 || blue_fade == -1) { // to do: remove
+            ALOGD("set_speaker_light_locked: LED ERROR\n");
+
+            red   = 127;
+            green = 127;
+            blue  = 127;
+
+            red_fade = green_fade = blue_fade = 2; // 0.52 => 0.26 for 127 color
+        }
+
+        sprintf(breath_pattern_red,   "%d %d %d %d", red_fade,   (int)(onMS/1000), red_fade,   (int)(offMS/1000));
+        sprintf(breath_pattern_green, "%d %d %d %d", green_fade, (int)(onMS/1000), green_fade, (int)(offMS/1000));
+        sprintf(breath_pattern_blue,  "%d %d %d %d", blue_fade,  (int)(onMS/1000), blue_fade,  (int)(offMS/1000));
+
     } else {
         write_int(RED_LED_FILE, red);
         write_int(GREEN_LED_FILE, green);
